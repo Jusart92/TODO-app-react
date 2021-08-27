47d8075655f0719121fa7334fef8fd4460628366
Arturo Juarez <jusart.92@gmail.com>
2021-08-27 00:22:00 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-// import logo from "./logo.svg";
-// import "./App.css";
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { CreateTodoButton } from "./CreateTodoButton";
+import { TodoItem } from "./TodoItem";
+import { TodoList } from "./TodoList";
+
+const todos = [
+  { text: "Cortat cebolla", complated: "false" },
+  { text: "Tormar el curso de intro a React", complated: "false" },
+  { text: "llorar con la llorona", complated: "false" },
+];
 
 function App() {
-  return "";
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ src/TodoCounter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TodoCounter() {
+  return <h2>Has completado 2 de 4 TODOs</h2>;
+}
+
+export { TodoCounter };
