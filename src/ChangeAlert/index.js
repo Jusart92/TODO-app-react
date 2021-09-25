2acc6ac5ef23cc17f7d63b98538a328f1c8e95e1
Arturo Juarez <jusart.92@gmail.com>
2021-09-25 11:16:37 -0500

    HOC css

--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { withStorageListener } from './withStorageListener';
-
+import './ChangeAlert.css';
+import '../TodoForm/TodoForm.css';
 function ChangeAlert({ show, toggleShow }) {
   if (show) {
     return (
-      <div>
-        <p>Hubo cambios</p>
-        <button onClick={() => toggleShow(false)}>
-          Volver a cargar la informacion
-        </button>
+      <div className='ChangeAlert-bg'>
+        <div className='ChangeAlert-container'>
+          <p>Parece que cambiaste tus TODOS en otra ventana</p>
+          <p>¿Quiere sincronizar sus Todos?</p>
+          <button
+            className='TodoForm-button TodoForm-button--add'
+            onClick={toggleShow}
+          >
+            Volver a cargar la informacion
+          </button>
+        </div>
       </div>
     );
   } else {
--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -14,7 +14,7 @@ function TodoList(props) {
         !props.searchedTodos.length &&
         props.onEmptySeachReasults(props.searchText)}
 
-      {props.searchedTodos.map(renderFunc)}
+      {!props.loading && !props.error && props.searchedTodos.map(renderFunc)}
       <ul></ul>
     </section>
   );
